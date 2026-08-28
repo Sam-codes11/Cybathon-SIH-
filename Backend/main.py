f506f032672064d8ee58733828367e438e9b4433
Suraj Maharana <surajkumarmaharana01@gmail.com>
2026-08-28 23:26:33 +0530

    CORS added

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,15 @@ from audio_routes import router as audio_router
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(audio_router)
 
 @app.get("/")
